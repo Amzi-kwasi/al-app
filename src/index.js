57e287389ef3b4c9030c1061e426af0d6ddd6eb7
Asvan-Launcher <95467525+Amzi-kwasi@users.noreply.github.com>
2026-03-22 07:00:17 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,19 +1,10 @@
-import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/home";
-
-
-export default function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route>
-         <Route path="/" element={<Home />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
-}
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
